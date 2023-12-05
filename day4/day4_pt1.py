25f17512de552f3e856bbbe96059c29c4dd52f9e
Maria Rodriguez Hertz <marurh20@gmail.com>
2023-12-04 20:01:52 -0500

    Add answer to day 4 pt 2

--- day4/day4_pt1.py
+++ day4/day4_pt1.py
@@ -1,16 +1,16 @@
 from math import floor
 from parse import parse
 
-class Game:
+class Card:
     def __init__(self, line):
         self.winning, self.yours = self.split_numbers(line)
-        self.score = self.score_game()
+        self.score = self.score_card()
     
     def split_numbers(self, line):
-        _, winning, yours = parse("Card {}: {} | {}", line.split("\n")[0])
+        _, winning, yours = parse("Card {}: {} | {}", line)
         return [i for i in winning.split(" ") if not i == '' ], [j for j in yours.split(" ") if not j == '']
 
-    def score_game(self):
+    def score_card(self):
         matches = 0
         for number in self.yours:
             if number in self.winning: matches+=1;
@@ -19,7 +19,7 @@ class Game:
 
 sum = 0
 for line in open("../input/input_4.txt","r"):
-    game = Game(line)
-    sum += game.score
+    card = Card(line.split("\n")[0])
+    sum += card.score
 
 print(sum)
---
+++ day4/day4_pt2.py
@@ -0,0 +1,36 @@
+from math import floor
+from parse import parse
+
+class Game:
+    def __init__(self, f):
+        self.cards = [line.split("\n")[0] for line in f.readlines()]
+        self.card_copies = [1]*len(self.cards)
+
+    def play(self):
+        for i in range(len(self.cards)):
+            card = Card(self.cards[i])
+
+            for j in range(1, card.score+1):
+                try: self.card_copies[i+j]+=self.card_copies[i]
+                except: break
+    
+        return sum(self.card_copies)
+            
+
+
+class Card:
+    def __init__(self, card_info):
+        self.winning, self.yours = self.split_numbers(card_info)
+        self.score = self.score_card()
+    
+    def split_numbers(self, card_info):
+        _, winning, yours = parse("Card {}: {} | {}", card_info)
+        return [i for i in winning.split(" ") if not i == '' ], [j for j in yours.split(" ") if not j == '']
+
+    def score_card(self):
+        return len([number for number in self.yours if number in self.winning])
+        
+
+game = Game(open("../input/input_4.txt","r"))
+print(game.play())
+
