d6510d4238135ba86bbf759d3317b553c0315bde
Maria Rodriguez Hertz <marurh20@gmail.com>
2023-12-06 18:10:57 -0500

    Add day6 answers

---
+++ day6/day6_pt2.py
@@ -0,0 +1,27 @@
+##########################################
+#  d = (t-s)*s = ts - s^2   for d=distance, t=time, s=speed
+#  s such that 0 < -s^2 + ts - d is when we win
+#  we will calculate this using the quadratic formula
+##########################################
+from math import sqrt, floor, ceil
+
+def quadraticFormula(a, b, c):
+    # given the problem, we don't need to handle the case where 
+    # the input to sqrt() is negative
+    pos = -b + sqrt(b**2 - (4*a*c))
+    neg = -b - sqrt(b**2 - (4*a*c))
+
+    return pos/(2*a), neg/(2*a)
+
+
+time, dist= [int(string.split(":")[1].replace(" ","")) for string in open('../input/input_6.txt', 'r').read().split('\n')]
+
+
+x, y = quadraticFormula(-1, time, -dist)
+
+if x >= y:
+    wins = ceil(x) - floor(y) - 1
+else:
+    wins = ceil(y) - floor(x) - 1
+
+print(wins)
