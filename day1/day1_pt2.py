de401ddc44b50f345ae514801b27274749fc88c1
Maria Rodriguez Hertz <marurh20@gmail.com>
2023-12-07 13:46:03 -0500

    Add regex solution for day1 pt2

--- day1/day1_pt2.py
+++ day1/day1_pt2.py
@@ -1,4 +1,5 @@
-input = open("../input/input_1.txt","r").read().split("\n")
+# input = open("../input/input_1.txt","r").read().split("\n")
+input = open("../input/test/test_input_1_2.txt","r").read().split("\n")
 
 numbers = {
     'zero': 0,
@@ -13,21 +14,49 @@ numbers = {
     'nine': 9
 }
 
-sum = 0
-for line in input:
+def naive(input):
+    sum = 0
+    for line in input:
 
-    # (index, num)
-    nums = []
+        # (index, num)
+        nums = []
 
-    for i in range(len(line)):
-        #digit
-        if line[i].isdigit(): nums.append((i, int(line[i]))); continue
-        #written
-        for j in range(3,7):
-            if line[i:i+j]in numbers: nums.append((i,numbers[line[i:i+j]])); break
+        for i in range(len(line)):
+            #digit
+            if line[i].isdigit(): nums.append((i, int(line[i]))); continue
+            #written
+            for j in range(3,7):
+                if line[i:i+j]in numbers: nums.append((i,numbers[line[i:i+j]])); break
+        
+        sum+=(nums[0][1]*10)
+        sum+=(nums[-1][1])
+
+    print(sum)
+
+# naive(input)
+
+# Using regex:
+from re import search
+
+def regex(input):
+    sum = 0
     
-    nums.sort()
-    sum+=(nums[0][1]*10)
-    sum+=(nums[-1][1])
+    for line in input:
+        first = search(r'\d|zero|one|two|three|four|five|six|seven|eight|nine', line).group(0)
+        last = search(r'\d|orez|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin', line[::-1]).group(0)
 
-print(sum)
+        try: 
+            first = int(first)
+        except:
+            first = numbers[first]
+
+        try: 
+            last = int(last)
+        except:
+            last = numbers[last[::-1]]
+
+        sum+=(first*10 + last)
+       
+    print(sum)
+
+regex(input)
